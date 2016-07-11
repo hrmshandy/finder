3b05239c080526d3e18d10bd58485189d56f6792
Shandy <hrm.shandy05@gmail.com>
2016-07-11 16:50:54 +0700

    first commit

---
+++ src/FinderServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Hrmshandy\Finder;
+
+use League\Glide\Server;
+use League\Glide\ServerFactory;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Filesystem\Filesystem;
+
+class FinderServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(Router $router)
+    {
+        $this->glideServer();
+        $this->defineRoute($router);
+    }
+
+    /**
+     * [glideServer description]
+     * @return [type] [description]
+     */
+    private function glideServer()
+    {
+        $this->app->singleton(Server::class, function ($app) {
+            $filesystem = $app->make(Filesystem::class);
+
+            return ServerFactory::create([
+                'source' => $filesystem->getDriver(),
+                'source_path_prefix' => '',
+                'cache' => $filesystem->getDriver(),
+                'cache_path_prefix' => '.cache',
+                //'base_url' => // Base URL of the images
+            ]);
+        });
+    }
+
+    /**
+     * [defineRoute description]
+     * @param  Router $router [description]
+     * @return [type]         [description]
+     */
+    private function defineRoute(Router $router)
+    {
+         $router->get('finder/img/{path}', function(Server $server, Request $request, $path) {
+
+            $server->outputImage($path, $request->all());
+
+        })->where('path', '[A-Za-z0-9\/\.\-\_]+');
+    }
+}
